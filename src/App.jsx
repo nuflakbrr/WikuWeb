a67e24f7a60812c3517e8ceb540ca9f44c9cc91b
Hifdzi Maulana <hifdzimaulana12@gmail.com>
2022-04-03 22:20:00 +0700

    creating navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
-import React from 'react'
+import React from "react";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div>
-      <h1 class="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+      <Navbar />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
