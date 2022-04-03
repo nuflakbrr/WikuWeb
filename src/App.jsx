72b3557efb3f94143fd98543a7fbc35a12b1241e
Naufal Akbar <kbrnugroho@gmail.com>
2022-04-03 21:07:44 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <h1 class="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+    </div>
+  )
+}
+
+export default App
