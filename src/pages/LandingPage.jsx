f270eb1cee7f01a5055f94dd0ac20ca605541fe1
Naufal Akbar <kbrnugroho@gmail.com>
2022-04-04 10:07:25 +0700

    creating elemet pages

---
+++ src/pages/LandingPage.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+import Navbar from '../components/Navbar'
+
+export default function LandingPage() {
+    return (
+        <>
+            <Navbar />
+            <section>
+                <div className="container">
+                    <div className="flex flex-wrap">
+                        <div className="w-full self-center px-4">
+                            awdasdw3
+                        </div>
+                        <img src="/LandingPage.jpg" alt="Landing Page Image" className="w-full h-80" />
+                    </div>
+                </div>
+            </section>
+            <section>
+                awdawdawdawd
+            </section>
+        </>
+    )
+}
---
+++ src/pages/index.js
@@ -0,0 +1,2 @@
+export { default as LandingPage } from './LandingPage'
+export { default as NotFound } from './404'
