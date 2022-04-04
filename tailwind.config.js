924cd4b7257517489094bac94a62243d5e11027a
Hifdzi Maulana <hifdzimaulana12@gmail.com>
2022-04-04 10:33:36 +0700

    embed a font

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
   ],
   darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'lato': ['Lato', 'sans-serif']
+      }
+    },
   },
   plugins: [],
 }
