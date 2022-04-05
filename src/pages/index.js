d31a5a25c6ae54002d35e082fb3981da30e8af50
Naufal Akbar <kbrnugroho@gmail.com>
2022-04-05 08:56:28 +0700

    create element register

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
 export { default as LandingPage } from './LandingPage'
 export { default as Login } from './Login'
+export { default as Register } from './Register'
 export { default as NotFound } from './404'
