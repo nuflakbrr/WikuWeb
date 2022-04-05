864082553093e5ce1c45841f44c9dd4a1999188d
Naufal Akbar <kbrnugroho@gmail.com>
2022-04-05 08:56:50 +0700

    create register elemet router

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 
-import { LandingPage, Login, NotFound } from './pages'
+import { LandingPage, Login, NotFound, Register } from './pages'
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<LandingPage />} />
       <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
   )
