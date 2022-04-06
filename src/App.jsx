81deb1c01093e1cf3dad6098131ae3a85332ff81
Naufal Akbar <kbrnugroho@gmail.com>
2022-04-07 06:50:44 +0700

    refactor

--- src/App.jsx
+++ src/App.jsx
@@ -9,8 +9,8 @@ function App() {
       <Route path="/" element={<LandingPage />} />
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
-      <Route path="*" element={<NotFound />} />
       <Route path="/biodata" element={<Biodata />} />
+      <Route path="*" element={<NotFound />} />
     </Routes>
   );
 }
