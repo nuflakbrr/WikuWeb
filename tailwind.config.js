6d202bc3f7095c959b74d1ebc28e2436acb76425
Hifdzi Maulana <hifdzimaulana12@gmail.com>
2022-04-23 21:19:48 +0700

    add color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,13 @@ module.exports = {
       },
       gridTemplateColumns: {
         'profile-cards': 'repeat(auto-fit, minmax(20.25rem, 1fr))'
+      },
+      colors: {
+        'carmine-pink': '#E74C3C',
+        'philippine-silver': '#B6B6B6',
+        'ghost-white': '#F9F9F9',
+        'vampire-black': '#070708',
+        'saffron': '#F8BC37'
       }
     },
   },
