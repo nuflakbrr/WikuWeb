f21d8d5be667cb12aca4b0f0e4389332ba8783ff
Naufal Akbar <kbrnugroho@gmail.com>
2022-05-10 14:58:46 +0700

    create admin login page

--- src/pages/admin/index.js
+++ src/pages/admin/index.js
@@ -1,3 +1,4 @@
+export { default as AdminLogin } from './AdminLogin'
 export { default as Dashboard } from './Dashboard'
 export { default as Biodata } from './Biodata'
 export { default as EditBiodata } from './EditBiodata'
